214842c5572d07ff39869a89ad30ab1417901e96
Tonny Tzeng <tonny.tzeng@intel.com>
2015-08-21 16:04:08 +0800

    Import my web utility files

---
+++ js/supports.js
@@ -0,0 +1,141 @@
+function getSpeechGreeting() {
+	var now = new Date();
+	var weekdays = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+	var today = months[now.getMonth()] + ' ' + now.getDate() + ' ' + weekdays[now.getDay()] + ', ' + (1900+now.getYear());
+	var greeting = 'http://translate.google.com/translate_tts?tl=en&q="Today is ' + today + '"';
+	var element = "<embed width=0 height=0 loop='false' hidden='false' autostart='true' src='" + greeting + "' />";
+	return element;
+}
+
+function getMyGlobalIp(cbSuccess, cbComplete, cbError) {
+	var jsoninfo = $.ajax({
+		url: "http://jsonip.com/?callback=?",
+		type: "GET",
+		dataType: "jsonp",
+		success: function(jsondata) { if (cbSuccess!=null) cbSuccess(jsondata.ip) },
+		error: function(request, error) { if (cbError) cbError(request, error) },
+		complete: function(jsoninfo, status) { if (cbComplete) cbComplete(jsoninfo, status) }
+	});
+};
+
+// The following script is excerpted from http://www.quirksmode.org/js/detect.html
+var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{
+			string: navigator.userAgent,
+			subString: "Chrome",
+			identity: "Chrome"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari",
+			versionSearch: "Version"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera",
+			versionSearch: "Version"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "iPhone",
+			identity: "iPhone/iPod"
+	 	},
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+};
+
+// The following script calls jQuery to remove Ads from myweb.hinet.net
+$('document').ready(function(){ $(parent.document).find('frameSet').attr("cols","*,0")});
