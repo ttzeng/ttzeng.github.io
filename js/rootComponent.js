5254a0964a9e6ef9f7ca4d0f93a7f02a7291bca1
Tonny Tzeng <tonny.tzeng@gmail.com>
2023-05-06 23:21:26 +0800

    Add Vue & Capacitor integration Signed-off-by: <tonny.tzeng@gmail.com>

---
+++ js/rootComponent.js
@@ -0,0 +1,12 @@
+export default {
+    // Use data() option to declare an object that represent reactive state of a component.
+    // Vue calls this function when creating a new component instance, the object is then added
+    // to its reactivity system to trigger reactive updates.
+    data() {
+        return {
+        }
+    },
+    // The template of the rendered DOM to the component.
+    template: /* html */`
+`
+};
---
+++ template/vueComponent.js
@@ -0,0 +1,41 @@
+export default {
+    // Use data() option to declare an object that represent reactive state of a component.
+    // Vue calls this function when creating a new component instance, the object is then added
+    // to its reactivity system to trigger reactive updates.
+    data() {
+        return {
+        }
+    },
+    // Register any imported components to create the tags associated with those components.
+    components: {
+    },
+    // Declare custom attributes as Props of the component.
+    props: {
+    },
+    // Declare custom events emitted by the component.
+    emits: {
+    },
+    // Declare watch callbacks on data changes.
+    watch: {
+    },
+    // Define cached computed properties to compose data from existing sources.
+    computed: {
+    },
+    // Methods can be accessed on component instances, or used in template expressions.
+    methods: {
+    },
+    // Hooks to be called at different stages of the instance's lifecycle.
+    created() {
+    },
+    beforeMount() {
+    },
+    mounted() {
+    },
+    beforeUnmount() {
+    },
+    unmounted() {
+    },
+    // The template of the rendered DOM to the component.
+    template: /* html */`
+`
+};
