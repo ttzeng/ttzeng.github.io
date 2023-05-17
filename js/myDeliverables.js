b13a76220794aaa0d55ed1e73fac1d93463e9311
Tonny Tzeng <tonny.tzeng@gmail.com>
2023-05-17 15:03:02 +0800

    Add released Git projects to deliverables Signed-off-by: <tonny.tzeng@gmail.com>

--- js/myDeliverables.js
+++ js/myDeliverables.js
@@ -1,22 +1,28 @@
 export default {
     data() {
         return {
-            articles: []
+            articles: [],
+            projects: [],
         }
     },
     mounted() {
         $.getJSON("/json/publishing.json", (data) => {
             this.articles = data.articles;
         }).done(() => {
-            $("#filterArticle").textinput();
-            $("[data-role='table']").table().filterable();
+            $("#inputFilter").textinput();
+            $("#articleList").table().filterable();
+        });
+        $.getJSON("/json/projects.json", (data) => {
+            this.projects = data.projects;
+        }).done(() => {
+            $("#projectList").table().filterable();
         });
     },
     template: /* html */`
     <form>
-        <input id="filterArticle" data-type="search">
+        <input id="inputFilter" data-type="search">
     </form>
-    <table data-role="table" data-filter="true" data-input="#filterArticle" class="table-stripe ui-responsive">
+    <table id="articleList" data-role="table" data-filter="true" data-input="#inputFilter" class="table-stripe ui-responsive">
         <thead>
             <tr>
                 <th>No.</th>
@@ -33,5 +39,23 @@ export default {
                 </tr>
             </template>
         </tbody>
-    </table>`
+    </table>
+    <table id="projectList" data-role="table" data-filter="true" data-input="#inputFilter" class="table-stripe ui-responsive">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Repository</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <template v-for="(proj, index) in projects">
+                <tr>
+                    <th>{{index+1}}</th>
+                    <td><a :href="proj.url" target="_blank">{{proj.url}}</a></td>
+                    <td>{{proj.description}}</td>
+                </tr>
+            </template>
+        </tbody>
+    </table-->`
 };
