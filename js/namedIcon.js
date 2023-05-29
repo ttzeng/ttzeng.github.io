03544d812ffc8c4fa7ad728080ec9872afb9ec14
Tonny Tzeng <tonny.tzeng@gmail.com>
2023-05-29 22:29:25 +0800

    Fix the text overflow of named icons Fix: #4 Signed-off-by: <tonny.tzeng@gmail.com>

--- js/namedIcon.js
+++ js/namedIcon.js
@@ -1,11 +1,19 @@
 export default {
     props: {
-        size: { type: Number, default: 48 },
+        size: { type: Number, default: 56 },
         icon: { type: String, required: true },
         link: { type: String, required: true }
     },
+    computed: {
+        textWidth() {
+            return {
+                "inline-size": this.size + "px",
+                "overflow-wrap": "break-word",
+            }
+        }
+    },
     template: /* html */`
     <div class="namedIcon">
-        <a :href="link"><img :height="size" :src="icon"/><slot/></a>
+        <a :href="link" :style="textWidth"><img :height="size" :src="icon"/><slot/></a>
     </div>`
 };
