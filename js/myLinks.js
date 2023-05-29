c3aca72cc3caf46de5f667193095933aeb812f54
Tonny Tzeng <tonny.tzeng@gmail.com>
2023-05-29 14:52:50 +0800

    Add knowledge named icons to myLinks ASigned-off-by: <tonny.tzeng@gmail.com>

--- js/myLinks.js
+++ js/myLinks.js
@@ -3,6 +3,8 @@ import namedIcon from "./namedIcon.js";
 export default {
     data() {
         return {
+            learnings: [],
+            technologies: [],
             banks: [],
             securities: []
         }
@@ -11,15 +13,32 @@ export default {
         namedIcon
     },
     mounted() {
+        $.getJSON("/json/knowledge.json", (data) => {
+            this.technologies = data.technologies;
+            this.learnings = data.learnings;
+        }).done(() => {
+            $("#groupKnowledge").collapsible();
+        });
         $.getJSON("/json/finance.json", (data) => {
             this.banks = data.banks;
             this.securities = data.securities;
         }).done(() => {
-            $("[data-role='collapsible']").collapsible();
+            $("#groupFinance").collapsible();
         });
     },
     template: /* html */`
-    <div data-role="collapsible">
+    <div id="groupKnowledge" data-role="collapsible">
+        <h1>知識</h1>
+        <div class="radiusNamedGroup">
+            <span class="name">技術</span>
+            <named-icon v-for="item in technologies" :link="item.url" :icon="item.icon">{{item.name}}</named-icon>
+        </div>
+        <div class="radiusNamedGroup">
+            <span class="name">學習</span>
+            <named-icon v-for="item in learnings" :link="item.link" :icon="item.icon">{{item.title}}</named-icon>
+        </div>
+    </div>
+    <div id="groupFinance" data-role="collapsible">
         <h1>理財</h1>
         <div class="radiusNamedGroup">
             <span class="name">銀行</span>
