ee026efd7a0b227395cd5af4e92125a5bc23ef71
Tonny Tzeng <tonny.tzeng@gmail.com>
2023-05-31 14:10:30 +0800

    Add a selector for filtering technology icons by domains Signed-off-by: <tonny.tzeng@gmail.com>

--- js/myLinks.js
+++ js/myLinks.js
@@ -5,6 +5,8 @@ export default {
         return {
             learnings: [],
             technologies: [],
+            techDomains: [],
+            domainSelected: "",
             banks: [],
             securities: []
         }
@@ -15,8 +17,12 @@ export default {
     mounted() {
         $.getJSON("/json/knowledge.json", (data) => {
             this.technologies = data.technologies;
+            var domainSet = new Set();
+            data.technologies.forEach(e => { domainSet.add(e.domain) });
+            this.techDomains = Array.from(domainSet);
             this.learnings = data.learnings;
         }).done(() => {
+            $("#domainSelector").selectmenu();
             $("#groupKnowledge").collapsible();
         });
         $.getJSON("/json/finance.json", (data) => {
@@ -31,7 +37,12 @@ export default {
         <h1>知識</h1>
         <div class="radiusNamedGroup">
             <span class="name">技術</span>
-            <named-icon v-for="item in technologies" :link="item.url" :icon="item.icon">{{item.name}}</named-icon>
+            <select id="domainSelector" v-model="domainSelected" data-mini="true">
+                <option v-for="domain in techDomains" :value="domain">{{domain}}</option>
+            </select>
+            <template v-for="item in technologies">
+                <named-icon v-show="item.domain === domainSelected" :link="item.url" :icon="item.icon">{{item.name}}</named-icon>
+            </template>
         </div>
         <div class="radiusNamedGroup">
             <span class="name">學習</span>
