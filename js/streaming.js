d09536e4d17ca321a23aa175f020964ee14b975d
Tonny Tzeng <tonny.tzeng@gmail.com>
2024-11-11 21:26:11 +0800

    CSS grid layout is more fit for video clips Close: #14 Signed-off-by: <tonny.tzeng@gmail.com>

--- js/streaming.js
+++ js/streaming.js
@@ -15,14 +15,13 @@ export default {
         });
     },
     template: /* html */`
-    <div v-for="channel in streaming" data-role="collapsible" data-inset="false" data-collapsed="false"
-         data-collapsed-icon="carat-d" data-expanded-icon="carat-u"
-         style="text-align: center">
-        <h1>{{channel.title}}</h1>
-        <iframe :src="channel.link" :title="channel.title"
-                width="400" height="225" frameborder="0"
+    <div class="media-grid">
+        <div v-for="channel in streaming">
+            {{channel.title}}
+            <iframe :src="channel.link" :title="channel.title" width="100%" height="360px" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-        </iframe>
+            </iframe>
+        </div>
     </div>`
 };
